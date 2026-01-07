0e39b1f6ee9e8e3768cccc3c96cb04ff78df0bf1
Daniela Lara <laradni@icloud.com>
2026-01-06 18:53:00 -0600

    (Add) People in the bus exercise

--- codewars/fundamentals/people_in_bus.go
+++ codewars/fundamentals/people_in_bus.go
@@ -1,12 +1,11 @@
 package fundamentals
 
-func Number(busStops [][2]int) int {
+func Number(busStops [][2]int) (peopleLeft int) {
 	onBus, offBus := 0, 0
 
 	for _, people := range busStops {
-		onBus += people[0]
-		offBus += people[1]
+		peopleLeft += people[0] - people[1]
 	}
 
-	return onBus - offBus
+	return
 }
