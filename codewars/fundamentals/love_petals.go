a0b4ed920d3b8e2e0c7b78e3e74e36a76bc0ca05
Daniela Lara <laradni@icloud.com>
2026-01-09 10:16:16 -0600

    (Add) Love Petals exercise

---
+++ codewars/fundamentals/love_petals.go
@@ -0,0 +1,14 @@
+package fundamentals
+
+func HowMuchILoveYou(i int) string {
+	phrases := []string{
+		"I love you",
+		"a little",
+		"a lot",
+		"passionately",
+		"madly",
+		"not at all",
+	}
+
+	return phrases[(i-1)%len(phrases)]
+}
