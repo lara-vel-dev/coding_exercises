74a96035df14fd9d1528c35fce3d12236c99ba23
Daniela Lara <laradni@icloud.com>
2026-01-09 18:13:09 -0600

    (Add) Sort array by string length exercise

---
+++ codewars/fundamentals/sorted_length_words.go
@@ -0,0 +1,11 @@
+package fundamentals
+
+import "sort"
+
+func SortByLength(arr []string) []string {
+	sort.Slice(arr, func(i, j int) bool {
+		return len(arr[i]) < len(arr[j])
+	})
+
+	return arr
+}
